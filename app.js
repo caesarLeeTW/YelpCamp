004db27315f7beb6d44fdc871efa59494cc07c31
Caesar <dev.wenhan@gmail.com>
2022-01-26 17:56:12 +0800

    Adding basic styles with Bootstrap5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/camground');
 const morgan = require('morgan');
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -39,7 +41,8 @@ app.post('/campgrounds', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground });
 })
 app.get('/campgrounds/:id/edit', async (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis eos cum blanditiis, et incidunt voluptatibus praesentium earum? Enim, eius rem? Dicta fugit iure blanditiis harum esse vitae rem placeat vero.',
+            price
         })
         await camp.save();
     }
